c54fc04334a833b1210b3f578cb4f3e7366b4324
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-02 17:39:23 +0530

    XAMARIN-34939 - multi range selection sample added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarViewModel
+    {
+       
+            public ObservableCollection<SelectionRange> SelectedRanges { get; set; }
+        public CalendarViewModel()
+        {
+            SelectedRanges = new ObservableCollection<SelectionRange>();
+            SelectedRanges.Add(new SelectionRange()
+            {
+                StartDate = new DateTime(2019, 09, 12),
+                EndDate = new DateTime(2019, 09, 26)
+            });
+
+            SelectedRanges.Add(new SelectionRange()
+            {
+                StartDate = new DateTime(2019, 09, 30),
+                EndDate = new DateTime(2019, 10, 04)
+            });
+        }
+    }
+}
+
